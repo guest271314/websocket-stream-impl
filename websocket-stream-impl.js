900443b44325d37152642a8c02c53b4bc48baea3
guest271314 <guest271314@gmail.com>
2025-05-31 04:34:53 +0000

    Update websocket-stream-impl.js

--- websocket-stream-impl.js
+++ websocket-stream-impl.js
@@ -2,6 +2,9 @@ class WebSocketStreamImpl {
   #ws;
   #readableController;
   #writableController;
+  #readable;
+  #writable;
+  #handleCloseEvent;
   #closedPromise = Promise.withResolvers();
   #openedPromise = Promise.withResolvers();
   opened;
@@ -19,112 +22,99 @@ class WebSocketStreamImpl {
         this.signal = options.signal;
         this.signal.addEventListener("abort", async (e) => {
           try {
-            this.#closedPromise.reject(
-              new DOMException("WebSocket handshake was aborted", "ABORT_ERR"),
-            );
-            this.#openedPromise.reject(
-              new DOMException("WebSocket handshake was aborted", "ABORT_ERR"),
-            );
-          } catch {
+            this.#closedPromise.reject(new DOMException("WebSocket handshake was aborted", "ABORT_ERR"));
+            this.#openedPromise.reject(new DOMException("WebSocket handshake was aborted", "ABORT_ERR"));
+          } catch (e) {
+            console.log(e);
           }
         });
       }
       this.#openedPromise.promise.catch(() => {});
-      this.closed = this.#closedPromise.promise;
-      this.opened = new Promise((r) => {
-        setTimeout(r, 1);
-      }).then(() =>
-        new Promise(async (resolve, reject) => {
+      this.#closedPromise.promise.catch(() => {});
+      this.closed = this.#closedPromise.promise.catch((e) => {
+        throw e;
+      });
+      this.opened = new Promise(async (resolve, reject) => {
+        try {
+          const aborted = this.signal?.aborted;
+          if (aborted) {
+            return reject(this.#openedPromise);
+          }
+        } catch {
+          return;
+        }
+        const args = [url, { protocols: this.protocols }];
+        if (this.protocols.length === 0) {
+          args.pop();
+        }
+        this.#handleCloseEvent = function handleCloseEvent(e) {
+          const { code, reason } = e;
+          console.log("close event", e);
           try {
-            const aborted = this.signal?.aborted;
-            if (aborted) {
-              throw null;
+            if (this.#readable.locked) {
+              this.#readableController.close();
             }
-          } catch {
-            this.#openedPromise.promise.catch((e) => reject(e));
-            return;
-          }
-          // For Deno, throws when protocols is empty Array []
-          const args = [url, { protocols: this.protocols }];
-          if (this.protocols.length === 0) {
-            args.pop();
-          }
-          this.handleCloseEvent = (e) => {
-            const { code, reason } = e;
-            console.log("close event", e);
-            try {
-              if (this.readable.locked) {
-                this.#readableController.close();
-              }
-              if (this.writable.locked) {
-                this.writable.close().catch(() => {});
-              }
-              this.#openedPromise.resolve({
-                readable: this.readable,
-                writable: this.writable,
-              });
-              this.#closedPromise.resolve({ closeCode: code, reason });
-            } catch (e) {
+            if (this.#writable.locked) {
+              this.#writable.close().catch(() => {});
             }
-          };
-          this.#ws = new WebSocket(...args);
-          this.#ws.binaryType = "arraybuffer";
-          this.#ws.addEventListener("close", this.handleCloseEvent);
-          this.#ws.addEventListener("error", (e) => {
-            this.#closedPromise.reject(e);
-          }, { once: true });
-          this.#ws.addEventListener("message", (e) => {
-            this.#readableController.enqueue(e.data);
+            this.#closedPromise.resolve({ closeCode: code, reason });
+          } catch (e) {}
+        };
+        this.#ws = new WebSocket(...args);
+        this.#ws.binaryType = "arraybuffer";
+        this.#ws.addEventListener("close", this.#handleCloseEvent.bind(this));
+        this.#ws.addEventListener("error", (e) => {
+          this.#closedPromise.reject(e);
+        }, { once: true });
+        this.#ws.addEventListener("message", (e) => {
+          this.#readableController.enqueue(e.data);
+        });
+        this.#ws.addEventListener("open", (e) => {
+          console.log(e, this);
+          this.#readable = new ReadableStream({
+            start: (c) => {
+              this.#readableController = c;
+            },
+            cancel: async (reason) => {
+              console.log(reason);
+              await this.#writable.close();
+            }
           });
-          this.#ws.addEventListener("open", (e) => {
-            this.readable = new ReadableStream({
-              start: (c) => {
-                this.#readableController = c;
-              },
-              cancel: async (reason) => {
-                await this.writer.close();
-              },
-            }, { once: true });
-            this.writable = new WritableStream({
-              start: (c) => {
-                this.#writableController = c;
-              },
-              write: (value) => {
-                this.#ws.send(value);
-              },
-              close: () => {
-                console.log("close");
-                this.#readableController.close();
-                this.#ws.close();
-              },
-              abort: (reason) => {
-              },
-            });
-
-            resolve({
-              readable: this.readable,
-              writable: this.writable,
-            });
+          this.#writable = new WritableStream({
+            start: (c) => {
+              this.#writableController = c;
+            },
+            write: (value) => {
+              this.#ws.send(value);
+            },
+            close: () => {
+              console.log("close");
+              this.#readableController.close();
+              this.#ws.close();
+            },
+            abort: (reason) => {}
           });
-        }).catch((e) => {
-          throw e;
-        })
-      );
+          resolve({
+            readable: this.#readable,
+            writable: this.#writable
+          });
+        });
+      }).catch((e) => {
+        throw e;
+      });
     } catch (e) {
       throw e;
     }
   }
   close({ code = 1000, reason = "" } = {}) {
-    // Node.js client doesn't automatically close connection
-    if (navigator.userAgent.includes("Node")) {
-      new Promise((r) => {
-        setTimeout(r, 50);     
-      }).then(() => this.handleCloseEvent({ code: 1000, reason: "Done streaming" }));
-    }
     console.log(this.#ws.readyState);
-    new Promise((r) => {
-      setTimeout(r, 50);
-    }).then(() => this.#ws.close(code, reason));
+    try {
+      this.#ws.close(code, reason);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.#handleCloseEvent({ code: 1000, reason: "Done streaming" });
+    }
   }
 }
 
